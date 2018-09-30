1630965dd5823909934798091bdf85c54a39909c
Fauzan Hilmy Abyan <fauzanhilmyabyan@gmail.com>
2018-09-30 20:28:54 +0700

    hellow world

---
+++ 1_introduction/helloworld.js
@@ -0,0 +1,8 @@
+/*
+Author  : ojancorleone
+Year    : 2018
+Version : 0.0
+*/
+
+/* is a function that writes a message to log on the debugging console (In Chrome or Firefox Debugging Mode)*/ 
+console.log("Hello World");
